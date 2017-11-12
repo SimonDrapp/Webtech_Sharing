f5d770e4d0f590d7f2e8e8d0c2b400b4d8c243a9
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2017-11-12 13:10:56 +0100

    - Vermietung-Seite komplett zu Ende gemacht - Responsive Design erledigt

--- resources/views/Vermieten.blade.php
+++ resources/views/Vermieten.blade.php
@@ -10,50 +10,26 @@
 
         <p class="VermietungÜberschrift">Stelle <span class="jetzt">jetzt</span> dein Auto oder dein Fahrrad mit wenigen Klicks zur Vermietung bereit!</p>
 
-        <div class="row">
-            <div class="col-md-6">
-                <img class="AutobildVermietung img-responsive" src="img/car2.jpg" data-src="holder.js/300x480/auto" alt="AutobildVermietung">
-                    <div class="bottonver">
-                        <button type="button" class="btn btn-basic1 btn-responsive" id="AutoButtonVermietung">Hier gehts zur Autovermietung<span
-                                    class="glyphicon glyphicon-triangle-right"></span></button>
-                    </div>
-            </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 col-lg-6">
+                    <img class="AutobildVermietung img-rounded" src="img/car2.jpg" data-src="holder.js/300x480/auto" alt="AutobildVermietung">
+                        <div class="bottonver">
+                            <button type="button" class="btn btn-basic1 btn-responsive" id="AutoButtonVermietung">Hier vermieten<span
+                                        class="glyphicon glyphicon-triangle-right"></span></button>
+                        </div>
+                </div>
 
 
-            <div class="col-md-6">
-                <img class="FahrradbildVermietung img-responsive" src="img/bike1.jpg" data-src="holder.js/300x480/auto" alt="FahrradbildVermietung">
-                    <div class="bottonver">
-                        <button type="button" class="btn btn-basic1 btn-responsive" id="FahrradButtonVermietung">Hier gehts zur Fahrradvermietung<span
-                                    class="glyphicon glyphicon-triangle-right"></span></button>
-                    </div>
+                <div class="col-md-12 col-lg-6">
+                    <img class="FahrradbildVermietung img-rounded" src="img/bike1.jpg" data-src="holder.js/300x480/auto" alt="FahrradbildVermietung">
+                        <div class="bottonver">
+                            <button type="button" class="btn btn-basic1 btn-responsive" id="FahrradButtonVermietung">Hier vermieten<span
+                                        class="glyphicon glyphicon-triangle-right"></span></button>
+                        </div>
+                </div>
             </div>
         </div>
-
- <!--           <div class="col-md-6">
-                <img class="FahrradbildVermietung img-responsive" src="img/bike1.jpg" data-src="holder.js/300x480/auto" alt="FahrradbildVermietung">
-            </div>
-       </div>
-
-        <div class="button">
-            <button type="button" class="btn btn-basic1 btn-responsive" id="AutoButtonVermietung">Zum Auto<span
-                        class="glyphicon glyphicon-triangle-right"></span></button>
-        </div>
--->
-
-  <!--      <div class="row">
-            <div class="col-md-offset-2">
-                <button type="button" class="btn btn-basic1 btn-responsive" id="AutoButtonVermietung">Zum Auto<span
-                            class="glyphicon glyphicon-triangle-right"></span></button>
-            </div>
-
-            <div class="col-md-offset-8">
-                <button type="button" class="btn btn-basic1 btn-responsive" id="FahrradButtonVermietung">Zum Fahrrad<span
-                            class="glyphicon glyphicon-triangle-right"></span></button>
-            </div>
-
-        </div>
-        -->
-
         @include('includes.footer')
     </body>
 
