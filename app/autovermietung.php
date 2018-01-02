18c5d0beeb538784f5a9f298551b13e816cd7484
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-02 16:25:52 +0100

    - Fahrradvermietenseite & Autovermietenseiten bearbeitet, sowohl mit CSS als auch Funktionalitäten. Nach der Eingabe auf der ersten Seite mit den jeweiligen Daten, werden diese auf der zweite Seite nochmal alle aufgelistet um diese auf Korrektheit zu überprüfen (mit Session). - Kalender nochmal teilweise bearbeitet (Datumsformat)

---
+++ app/autovermietung.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class autovermietung extends Model
+{
+    protected $table = 'autovermietung';
+    protected $primaryKey = 'id';
+    protected $keyType = 'Integer';
+    public $incrementing = true;
+    public $fillable = array('preis', 'marke', 'modell', 'farbe','kraftstoff','bild','details','startdate', 'enddate', 'postleitzahl', 'ort', 'strasseNr');
+}
---
+++ database/migrations/2018_01_02_121058_create_sharing_fahrradvermietung.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSharingFahrradvermietung extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fahrradvermietung', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('art');
+            $table->String('marke');
+            $table->String('modell');
+            $table->String('farbe');
+            $table->String('preis');
+            $table->String('bild');
+            $table->String('details');
+            $table->String('postleitzahl');
+            $table->String('ort');
+            $table->String('strasseNr');
+            $table->date('startdate');
+            $table->date('enddate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fahrradvermietung');
+    }
+}
