80aec4a240c52c064fe6f055b706a1b522314ef2
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-10 12:15:42 +0100

    - Letztes Commit

---
+++ app/Http/Controllers/VermietenController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VermietenController extends Controller
+{
+
+    public function check(){
+        return view('Vermieten');
+    //    return redirect('/Vermieten',200);
+     //   return response('lol',200);
+    }
+}
---
+++ resources/views/includes/header2.blade.php
@@ -0,0 +1,54 @@
+
+<header>
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container-fluid">
+            <a href="/" class="navbar-brand">
+                <img id="brand-image" src="img/logo_new.png" alt="Logo">
+                <h3 id="brand-text">my-easysharing</h3>
+            </a>
+
+
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right ">
+                    <li class="active"><a href="/">Startseite</a></li>
+                    <li><a href="{{ route('Vermieten') }}">Vermieten</a></li>
+                    <li class="dropdown">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sprachen<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Deutsch</a></li>
+                            <li><a href="#">Englisch</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
