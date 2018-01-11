231408a5e99e7ca1e9bcadce3893071d31018f1a
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-11 18:11:48 +0100

    - Problem mit Vermietenseite vom Auto behoben

--- resources/views/AnsichtAuto.blade.php
+++ resources/views/AnsichtAuto.blade.php
@@ -22,7 +22,7 @@
     </ol>
     <div class="carousel-inner">
         <div class="item active">
-            <img src="{{ asset('img/searchPictures/'.$vermietungen->autobild)}}" alt="Bild vom Auto1">
+            <img src="{{ asset('img/searchPictures/'.$vermietungen->bild)}}" alt="Bild vom Auto1">
         </div>
         <div class="item">
             <img src="{{asset('img/header1.jpg')}}" alt="Bild vom Auto2">
@@ -69,21 +69,21 @@
     <div class="row">
         <div class="col-md-4 col-lg-6 eigenschaft">
             <p><b>Marke:</b></p>
-            <p>{{$vermietungen->automarke}}<br><br></p>
+            <p>{{$vermietungen->marke}}<br><br></p>
             <p><b>Modell:</b></p>
-            <p>{{$vermietungen->automodell}}<br><br></p>
+            <p>{{$vermietungen->modell}}<br><br></p>
             <p><b>Autofarbe:</b></p>
-            <p>{{$vermietungen->autofarbe}}<br><br></p>
+            <p>{{$vermietungen->farbe}}<br><br></p>
             <p><b>Kraftstoff:</b></p>
-            <p>{{$vermietungen->autokraftstoff}}<br><br></p>
+            <p>{{$vermietungen->kraftstoff}}<br><br></p>
             <p><b>Baujahr:</b></p>
-            <p>{{$vermietungen->autobaujahr}}<br><br></p>
+            <p>{{$vermietungen->baujahr}}<br><br></p>
             <p><b>Details:</b></p>
-            <p>{{$vermietungen->autodetails}}<br><br></p>
+            <p>{{$vermietungen->details}}<br><br></p>
             <p><b>Preis pro Tag:</b></p>
-            <p>{{$vermietungen->autopreis}} €<br><br></p>
+            <p>{{$vermietungen->preis}} €<br><br></p>
             <p><b>Standort:</b><br></p>
-            <p>{{$vermietungen-> autostrasseNr}}, {{$vermietungen-> autopostleitzahl}} {{$vermietungen-> autoort}}</p>
+            <p>{{$vermietungen-> strasseNr}}, {{$vermietungen-> postleitzahl}} {{$vermietungen-> ort}}</p>
         </div>
 
         <div class="col-md-4 col-lg-6">
@@ -105,7 +105,7 @@
 
                     var geocoder = new google.maps.Geocoder();
                     geocoder.geocode({
-                        address: '{{$vermietungen->autoort}},{{$vermietungen->autopostleitzahl}},{{$vermietungen->autostrasseNr}}'
+                        address: '{{$vermietungen->ort}},{{$vermietungen->postleitzahl}},{{$vermietungen->strasseNr}}'
                     }, function (geocoderResults, status) {
                         if (status === 'OK') {
                             var latlng = geocoderResults[0].geometry.location;
@@ -113,7 +113,7 @@
                                 map: map,
                                 position: new google.maps.LatLng(latlng.lat(), latlng.lng()),
                                 icon: '/img/car.png',
-                                title: "{{$vermietungen->autostrasseNr}}, {{$vermietungen->autopostleitzahl}} {{$vermietungen->autoort}}"
+                                title: "{{$vermietungen->strasseNr}}, {{$vermietungen->postleitzahl}} {{$vermietungen->ort}}"
                             });
                         }
                     })
