af8bc364e21cbfbc7666dbf60bffa8d55ee569d6
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-19 17:34:59 +0100

    - Fehler

--- app/Http/Controllers/AutovermietungController.php
+++ app/Http/Controllers/AutovermietungController.php
@@ -44,10 +44,10 @@ class AutovermietungController extends Controller
     public function putCar(Request $request){
         $autovermietungen = new autovermietung;
         $marke = $request->marke . " ";
-        $marke2 = substr($marke,2, (sizeof($marke)-2));
+        $marke2 = substr($marke,2, (count($marke)-2));
         $autovermietungen->marke = $marke2;
         $modell = $request->modell . " ";
-        $modell2 = substr($modell,2, (sizeof($modell)-2));
+        $modell2 = substr($modell,2, (count($modell)-2));
         $autovermietungen->modell = $modell2;
         $autovermietungen->baujahr = $request->baujahr;
         $autovermietungen->farbe = $request->farbe;
