8f0299b723dc36954e2a5c1909cc7bd5f6c01084
Simon Drapp <si241dra@htwg-konstanz.de>
2018-01-21 22:21:07 +0100

    aboutUs

--- resources/views/überUns.blade.php
+++ resources/views/überUns.blade.php
@@ -16,9 +16,69 @@
     @include('includes.header')
 @endif
 
+<div class="container">
+    <div class="stat">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="milestone-counter">
+                <i class="fa fa-user fa-3x"></i>
+                <span class="stat-count highlight" data-count="122">0</span>
+                <div class="milestone-details">Besucher Webseite</div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="milestone-counter">
+                <i class="fa fa-list-alt fa-3x"></i>
+                <span class="stat-count highlight" data-count="14">0</span>
+                <div class="milestone-details">Angebote</div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="milestone-counter">
+                <i class="fa fa-car fa-3x"></i>
+                <span class="stat-count highlight" data-count="232">0</span>
+                <div class="milestone-details">Erfolge</div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="milestone-counter">
+                <i class="fa fa-coffee fa-3x"></i>
+                <span class="stat-count highlight" data-count="1000">0</span>
+                <div class="milestone-details">Ordered Coffee's</div>
+            </div>
+        </div>
+    </div><!-- stat -->
+</div>
 
 
 
+
+
+<script>
+
+    $('.stat-count').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({ countNum: $this.text()}).animate({
+                countNum: countTo
+            },
+
+            {
+                duration: 8000,
+                easing:'linear',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
+    });
+
+
+</script>
 </body>
 @include('includes.footer')
 </html>
