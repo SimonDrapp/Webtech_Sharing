3aee1d399a7896ab868160d69db1606e408c1e53
x-THINK\x <caroline.hess@yahoo.de>
2018-01-22 22:49:14 +0100

    -Footer versucht am boden zu befestigen

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="footer">
     <nav class="navbar navbar-inverse navbar-bottom">
         <div class="container-fluid2">
             <ul class="nav navbar-nav">
@@ -10,6 +10,18 @@
         </div>
     </nav>
 </footer>
+<script>
+    $(document).ready(function() {
+
+        var docHeight = $(window).height();
+        var footerHeight = $('.footer').height();
+        var footerTop = $('.footer').position().top + footerHeight;
+
+        if (footerTop < docHeight) {
+            $('.footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+        }
+    });
+</script>
 <!-- contact form -->
 @include('includes.Kontakt')
 
