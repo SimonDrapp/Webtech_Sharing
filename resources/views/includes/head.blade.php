ab993d2f5e1f2d7fe3d6ca1cb379796d53be3c74
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-23 14:37:18 +0100

    - Problem Kalender behoben

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -25,7 +25,6 @@
 <script type="text/javascript" src="/../../js/Kontakt.js"></script>
 <script type="text/javascript" src="/../../js/Bildauswahl.js"></script>
 <script type="text/javascript" src="/../../js/Bildauswahl.js"></script>
-<script type="text/javascript" src="/../../js/Kalender.js"></script>
 
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js"></script>
