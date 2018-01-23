dc838bb868213bd930a912d22751c11946afdecf
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-23 14:53:19 +0100

    - Probleme

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,7 +10,18 @@
         </div>
     </nav>
 </footer>
+<script>
+    $(document).ready(function() {
 
+        var docHeight = $(window).height();
+        var footerHeight = $('.footer').height();
+        var footerTop = $('.footer').position().top + footerHeight;
+
+        if (footerTop < docHeight) {
+            $('.footer').css('margin-top', (docHeight - footerTop) + 'px');
+        }
+    });
+</script>
 <!-- contact form -->
 @include('includes.Kontakt')
 
