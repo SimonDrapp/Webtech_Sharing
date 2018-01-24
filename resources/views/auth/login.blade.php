b37ef765a0541d7975b46e70359bb3c6252124c5
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-24 16:07:27 +0100

    - Namen geändert

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     @include('includes.head')
-    <title>my-easysharing | Login </title>
+    <title>my-easysharing | Einloggen </title>
       <!--<link rel="stylesheet" type="text/css" href="css/app.css">-->
     <link rel="stylesheet" type="text/css" href="css/login.css">
 
@@ -17,14 +17,14 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2 ">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">Einloggen</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail Addresse</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email"
@@ -39,7 +39,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Passwort</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -66,7 +66,7 @@
                        <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    Einloggen
                                 </button>
 <!--
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
