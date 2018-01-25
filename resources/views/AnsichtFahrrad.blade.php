3111a44d8646e1c6f23cb8c9d273756d6b9c2d3e
x-THINK\x <caroline.hess@yahoo.de>
2018-01-25 13:45:39 +0100

    -Fahrradansicht

--- resources/views/AnsichtFahrrad.blade.php
+++ resources/views/AnsichtFahrrad.blade.php
@@ -19,11 +19,41 @@
 
 <?php
 $lala = $vermietungen->bild;
-echo $lala;
 $result = explode(", ", $lala);
-echo $result[0];echo $result[1];echo $result[2];
+
+
 
 ?>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        @foreach($result as $val)
+            <li data-target="#myCarousel" data-slide-to="0"></li>
+        @endforeach
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="{{ asset('img/searchPictures/'.$result[0])}}" alt="<?php echo $result2; ?>">
+        </div>
+
+            <div class="item">
+                <img src="{{asset('img/searchPictures/'.$result[1])}}" alt="<?php echo $value;?>">
+            </div>
+        <div class="item">
+            <img src="{{asset('img/searchPictures/'.$result[2])}}" alt="<?php echo $value;?>">
+        </div>
+
+    </div>
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 
 <form action="{{ route('Bezahlen') }}" method="post">
     {{csrf_field()}}
