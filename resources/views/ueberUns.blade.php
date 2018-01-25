501e6cd63b01f013334b214da99eea706e1b7969
DESKTOP-8TOLE3H\rindr <rindrit.b@hotmail.de>
2018-01-25 13:02:37 +0100

    - Footer bearbeitet mit sozialen Netzwerken

--- resources/views/ueberUns.blade.php
+++ resources/views/ueberUns.blade.php
@@ -92,7 +92,7 @@
         </div><!-- stat -->
     </div>
 </div>
-
+<!--
 <div class="container-fluid">
     <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -106,7 +106,7 @@
             </div>
        </div>
 </div>
-
+-->
 <script>
     $stopAppear = 1;
 
